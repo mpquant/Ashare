f0d46f0bbf09205e0c195ade5470117cb1ce653c
dev <82923714+mpquant@users.noreply.github.com>
2022-03-13 10:41:56 +0800

    Update Ashare.py
    修正提示错误

--- Ashare.py
+++ Ashare.py
@@ -38,8 +38,11 @@ def get_price_sina(code, end_date='', count=10, frequency='60m'):    #新浪全�
         #print(code,end_date,count)    
     URL=f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale={ts}&ma=5&datalen={count}' 
     dstr= json.loads(requests.get(URL).content);       
-    df=pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'],dtype='float') 
-    df.day=pd.to_datetime(df.day);    df.set_index(['day'], inplace=True);     df.index.name=''      #处理索引                 
+    #df=pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'],dtype='float') 
+    df= pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'])
+    df['open'] = df['open'].astype(float); df['high'] = df['high'].astype(float);                          #转换数据类型
+    df['low'] = df['low'].astype(float);   df['close'] = df['close'].astype(float);  df['volume'] = df['volume'].astype(float)    
+    df.day=pd.to_datetime(df.day);    df.set_index(['day'], inplace=True);     df.index.name=''            #处理索引                 
     if (end_date!='') & (frequency in ['240m','1200m','7200m']): return df[df.index<=end_date][-mcount:]   #日线带结束时间先返回              
     return df
 
